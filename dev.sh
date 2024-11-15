bfce9912266d2fa087ee7039c9f62bfd1101513f
Bruno França <bruno@franca.xyz>
2024-11-15 17:33:25 +0000

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,25 +1,41 @@
-### ZK Sync Dev Setup ###
+#!/usr/bin/env bash
 
-# Clone other repos
-git clone https://github.com/matter-labs/era-consensus.git
-git clone https://github.com/matter-labs/era-contracts.git
+### Dev Box Setup ###
+main() {
+  # ZKsync Dev Setup
+  curl -L https://raw.githubusercontent.com/matter-labs/zksync-era/main/docs/src/guides/setup-dev.sh | bash
 
-# Cargo-deny
-cargo install cargo-deny --locked
+  # Cargo-deny (for era-consensus)
+  say "Installing Cargo-deny..."
+  cargo install cargo-deny --locked
 
-### Bruno's Dev Setup ###
+  # Clone more repos
+  say "Cloning more ZKsync repositories..."
+  git clone https://github.com/matter-labs/era-consensus.git
+  git clone https://github.com/matter-labs/era-contracts.git
 
-# Github
-sudo apt install gh
+  # Github CLI
+  say "Installing Github CLI..."
+  sudo apt install --yes gh
 
-# VS Code
-sudo apt install --yes wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-sudo apt update
-sudo apt install --yes code
+  # VS Code
+  say "Installing VS Code..."
+  sudo apt install --yes wget gpg
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+  rm -f packages.microsoft.gpg
+  sudo apt update
+  sudo apt install --yes code
 
-# OhMyBash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+  # OhMyBash
+  say "Installing OhMyBash..."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+}
+
+say() {
+  echo -e "\033[1;32m$1\033[0m"
+}
+
+main "$@"
+

5fc64e75b52511e01597fde462a26423a1367451
Bruno França <bruno@franca.xyz>
2025-03-04 19:50:54 +0000

    gh

--- dev.sh
+++ dev.sh
@@ -36,6 +36,10 @@ main() {
   git config --global user.name "Bruno França"
   git config --global user.email "bruno@franca.xyz"
 
+  # Github CLI
+  say "Installing Github CLI..."
+  sudo apt install --yes gh
+
   # Sublime Merge
   say "Installing Sublime Merge..."
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null

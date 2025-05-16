2bcfdf7be006390f4cea37931e438e44229a0ecc
Bruno França <bruno@franca.xyz>
2025-05-16 19:30:39 +0100

    Update dev1.sh

--- dev1.sh
+++ dev1.sh
@@ -23,8 +23,14 @@ main() {
 
   # Install Docker
   say "Installing Docker..."
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
   sudo apt install --yes docker-ce
   sudo usermod -aG docker ${USER}
 

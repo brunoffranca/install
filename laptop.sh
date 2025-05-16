255fc2f16edef88810ee85bd007196e65e171647
Bruno França <bruno@franca.xyz>
2025-05-16 17:34:02 +0100

    Update laptop.sh

--- laptop.sh
+++ laptop.sh
@@ -5,7 +5,18 @@ main() {
   # Packages
   say "Installing packages..."
   sudo apt update
-  sudo apt install --yes transmission libreoffice-calc libreoffice-writer
+  sudo apt install --yes transmission libreoffice-calc libreoffice-writer libfuse2t64
+
+   # Brave
+  say "Installing Brave..."
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install --yes brave-browser
+
+  # Bing Wallpaper
+  say "Installing Bing Wallpaper..."
+  sudo apt install --yes gnome-shell-extension-manager
 
   # Installing Rclone
   say "Installing Rclone..."
@@ -19,30 +30,6 @@ main() {
   rclone config create proton protondrive username=brunoffranca password=$(rclone obscure "$password")
   rclone sync proton:Thinkpad Proton # First sync here
   (crontab -l; echo "0 * * * * rclone sync Proton proton:Thinkpad") | crontab -
-
-  # Brave
-  say "Installing Brave..."
-  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-  sudo apt install --yes brave-browser
-
-  # Spotify
-  say "Installing Spotify..."
-  curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-  echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-  sudo apt update
-  sudo apt install --yes spotify-client
-
-  # Proton VPN
-  say "Installing Proton VPN..."
-  wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
-  sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
-  sudo apt install --yes proton-vpn-gnome-desktop libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
-
-  # Bing Wallpaper
-  say "Installing Bing Wallpaper..."
-  sudo apt install --yes gnome-shell-extension-manager
 }
 
 say() {
